b612e5020b563cfe57f847cccaaa2383e0fd063a
Trevor Muñoz <trevor.munoz@gmail.com>
2014-11-21 12:43:26 -0500

    More tests for MenuItem

--- test/tests/item.spec.js
+++ test/tests/item.spec.js
@@ -18,6 +18,20 @@ define([
                 var item = new MenuItem();
                 expect(item).to.be.ok;
             });
+
+            it('can be instantiated w/supplied values', function() {
+                var item = new MenuItem({
+                                    dish_name: 'Consomme printaniere royal',
+                                    dish_id: 1,
+                                    item_id: 73003
+                                    });
+
+                expect(item).to.be.ok;
+                expect(item.get('dish_name')).to.equal('Consomme printaniere royal');
+                expect(item.get('dish_id')).to.equal(1);
+                expect(item.get('item_id')).to.equal(73003);
+
+            });
         });
     });
 
