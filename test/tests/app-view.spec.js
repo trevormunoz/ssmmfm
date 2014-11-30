6f85f8744ac500d7293e30950e087529a4ac9489
Trevor Muñoz <trevor.munoz@gmail.com>
2014-11-30 10:26:22 -0500

    Add comment about subview convention

--- test/tests/app-view.spec.js
+++ test/tests/app-view.spec.js
@@ -30,6 +30,9 @@ define([
                 expect(this.app.$el.selector).to.equal('#app-content');
             });
 
+            // Using convention of creating object called 'subviews' on
+            // this main "app" view to hold references to subviews, 
+            // each of which get created during this view's initialization.
             it("should initialize the cluster subview", function() {
                 expect(this.app.subviews.cluster).to.be.ok;
             });
