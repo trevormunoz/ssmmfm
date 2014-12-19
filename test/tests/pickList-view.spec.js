e0c5332474d9945375e2f645e194587c9c5e9e4f
Trevor Muñoz <trevor.munoz@gmail.com>
2014-12-19 15:08:50 -0500

    New tests for event listener on PickListView

--- test/tests/pickList-view.spec.js
+++ test/tests/pickList-view.spec.js
@@ -32,5 +32,51 @@ define([
                 expect(this.view.collection).to.be.ok;
             });
         });
+
+        describe("initialization", function() {
+
+            beforeEach(function() {
+                this.spy = sinon.spy(PickListView.prototype, 'listenTo');
+                this.context = {
+                    listenTo: sinon.stub().returns(context),
+                    clusterBootstrapped: ['id1', 'id2', 'id3']
+                }
+                var cluster = new Cluster();
+                this.view = new PickListView({collection: cluster});
+            });
+
+            afterEach(function() {
+                PickListView.prototype.listenTo.restore();
+                this.view.remove();
+                this.view = null;
+            });
+
+            it("should setup an event listener", function() {
+                expect(this.spy.callCount).to.equal(1);
+
+                var listenerArgs = this.spy.args[0];
+                expect(listenerArgs[1]).to.equal('clusterBootstrapped');
+            });
+        });
+
+        describe("load cluster function", function() {
+
+            it("should respond to clusterBootstrapped event", function() {
+                var loadSpy = sinon.spy(PickListView.prototype, 'loadCluster');
+                var context = {
+                    loadCluster: sinon.stub().returns(context),
+                    clusterBootstrapped: 'something'
+                }
+
+                var cluster = new Cluster();
+                var view = new PickListView({collection: cluster});
+
+                expect(loadSpy).to.not.have.been.called;
+                Backbone.trigger('clusterBootstrapped');
+                expect(loadSpy.callCount).to.equal(1);
+                
+                PickListView.prototype.loadCluster.restore();
+            });
+        });
     });
 });
