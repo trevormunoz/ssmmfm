8b210569ba95a00daa83ac87118e7df538c80479
Trevor Muñoz <trevor.munoz@gmail.com>
2014-12-20 20:25:57 -0500

    Pseudo-code (as comments) outlining bootstrapping flow

--- src/js/views/app-view.js
+++ src/js/views/app-view.js
@@ -19,11 +19,37 @@ function(Backbone, ClusterView, MessageView) {
             
             var messageView = new MessageView();
             this.subviews.messages = messageView;
-            }
+
+        },
+
+        bootstrapCluster: function() {
+            //Pick a random number between 0 & length of
+            // document collection
             
-        });
+
+            // Build up a query using this random id as seed
+            
+
+            // Issue a full text search against all documents using
+            // a random scoring function --- i.e., get 10 random docs
+
+
+            // 10 documents is default response length; 
+            // pick one, again at random, and use its fingerprint
+            // value to define first cluster
+            
+
+                // Trigger an event on Backbone & send seed value
+                
+
+            // If something goes wrong with the request,
+            // trigger a failure event on Backbone
+            
+        },
+            
+    });
      
-return AppView
+    return AppView
        
-        });
+});
     
