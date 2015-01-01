7e8764d26be928edea03ce9fd9ba57f5cab16cee
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-01 14:56:42 -0500

    Existing sample code published to other branches

--- src/js/views/pickList-view.js
+++ src/js/views/pickList-view.js
@@ -7,6 +7,16 @@ define([
     
     var PickListView = Backbone.View.extend ({
         el: '#picker',
+
+        initialize: function() {
+            this.listenTo(Backbone, 'facetQuerySuccess', this.buildCluster);
+        },
+
+        buildCluster: function(data) {
+            var queryString = data;
+            this.collection.fetch({data: {source: queryString}, reset: true});
+
+        },
         
         });
         
