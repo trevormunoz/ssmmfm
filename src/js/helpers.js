91e86631d2cb45aba2de5907e07d5180ebc8386e
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-03 20:31:57 -0500

    Keep helper functions in a separate file

---
+++ src/js/helpers.js
@@ -0,0 +1,19 @@
+function timeStamp() {
+    var now = new Date();
+    var date = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
+    var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+
+    for ( var i = 1; i < 3; i++ ) {
+        if ( time[i] < 10 ) {
+            time[i] = "0" + time[i];
+        }
+    }
+
+    for ( var i = 1; i < 3; i++ ) {
+        if ( date[i] < 10 ) {
+            date[i] = "0" + date[i];
+        }
+    }
+
+    return date.join('-') + "T" + time.join(':');
+}
