fd7f785d2196b2f96ddccb3a536d407195571f77
Trevor Muñoz <trevor.munoz@gmail.com>
2015-03-05 17:55:01 -0500

    WIP on save via POST to /_bulk; TODO: Fix CORS

--- src/js/collections/index.js
+++ src/js/collections/index.js
@@ -15,7 +15,29 @@ define([
         },
 
         save: function() {
-            // Do stuff
+            
+            // Turn the array of models into an
+            // array of actions for bulk upload
+            var forUpload = _.map(this.models, 
+                    function(model){
+                        var esAction = {
+                            "index": "public_fare",
+                            "type": "term",
+                            "_source": model
+                        };
+
+                        return JSON.stringify(esAction);
+                    });
+            
+            var savePromise = $.ajax({
+                type: 'POST',
+                url: 'http://54.165.158.184/_bulk',
+                data: $.param({index: forUpload})
+            });
+
+            savePromise.done(function(data) {
+                window.console.log(data);
+            });
         },
     });
 
