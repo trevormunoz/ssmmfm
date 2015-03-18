301c42d998d1c02f841f7ac9e3d1a3a827b3d90b
Trevor Muñoz <trevor.munoz@gmail.com>
2015-03-18 17:30:01 -0400

    Tests passing again for cluster view

--- test/tests/cluster-view.spec.js
+++ test/tests/cluster-view.spec.js
@@ -3,112 +3,61 @@
 define([
         'sinon',
         'src/js/collections/seeds',
-        'src/js/views/cluster-view',
-        'fixtures/es/fixture',
-        'helpers/fakeServer-helper'
+        'src/js/views/cluster-view'
 ], function(sinon, Seeds, ClusterView) {
     'use strict';
 
     describe("View: Cluster", function(){
-        // describe("creation", function() {
-
-        //     beforeEach(function() {
-        //         var seedsCollex = new Seeds();
-        //         this.view = new ClusterView({collection: seedsCollex});
-        //     });
-
-        //     afterEach(function() {
-        //         this.view.remove();
-        //         this.view = null;
-        //     });
-
-        //     it("should exist", function() {
-        //         expect(this.view).to.be.ok;
-        //     });
-
-        //     it("should target the correct selector for binding", function() {
-        //         expect(this.view.$el.selector).to.equal('#cluster');
-        //     });
-
-        //     it("should be backed by a collection", function(){
-        //         expect(this.view.collection).to.be.ok;
-        //     });
-
-        //     it("should initialize a subviews object", function(){
-        //         expect(this.view.subviews).to.be.ok;
-        //         expect(this.view.subviews).to.be.instanceOf(Object);
-        //     });
-
-        //     it("should initialize a subview for the pick list", function() {
-        //         expect(this.view.subviews.picker).to.be.ok;
-        //     });
-
-        //     it("should initialize a subview for the item viewer", function() {
-        //         expect(this.view.subviews.viewer).to.be.ok;
-        //     });
-
-        //     it("should initialize a subview for the index output", function() {
-        //         expect(this.view.subviews.index).to.be.ok;
-        //     });
-
-        //     it('should have an events hash', function () {
-        //         expect(this.view.events).to.be.ok;
-        //     });
-
-        //     it('should have the correct event listeners', function() {
-        //         var eventsHash = this.view.events;
-        //         expect(eventsHash['click .modal-footer button']).to.be.ok;
-        //         expect(eventsHash['click .modal-footer button']).to.equal('closeInputModal');
-        //         expect(this.view.closeInputModal).to.be.instanceOf(Function);
-        //         expect(eventsHash['click tr.variant a']).to.be.ok;
-        //         expect(eventsHash['click tr.variant a']).to.equal('showContextModal');
-        //         expect(this.view.showContextModal).to.be.instanceOf(Function);
-        //     });                        
-        // });
-
-        describe('listeners', function () {
-           
-            it("should setup event listeners", function() {
-                this.fixture = this.fixtures.ClusterView.valid;
-                this.server = sinon.fakeServer.create();
-                this.server.respondWith(
-                        "GET",
-                        "http://api.publicfare.org/menus/item/_search",
-                        this.validResponse(this.fixture)
-                    );
-                this.server.autoRespond = true;
-                
-                var listenerSpy = sinon.spy(ClusterView.prototype, 'listenTo');
-                var seedsCollex = new Seeds();
-                var view = new ClusterView({collection: seedsCollex});
-                
-                expect(listenerSpy.callCount).to.equal(2);
-                
-                // sinon.spy.args returns an array of args; 
-                // arg to the listenTo function is itself 
-                // an array of 3 arguments
-                var listenerArgs = listenerSpy.args[0];
-                expect(listenerArgs[1]).to.equal('seedQuerySuccess');
-
-                view.remove();
-                view = null;
-                this.server.restore();
-                ClusterView.prototype.listenTo.restore();
-            });
-
-        });
-
-        describe('initialization', function () {
+        describe("initialization", function() {
 
             beforeEach(function() {
-                var seedsCollex = new Seeds();
-                this.view = new ClusterView({collection: seedsCollex});
+                var seedsStub = sinon.createStubInstance(Seeds);
+                this.view = new ClusterView({collection: seedsStub});
             });
 
             afterEach(function() {
-                this.view.remove();
+                this.view.cleanUp();
                 this.view = null;
             });
+
+            it("should exist", function() {
+                expect(this.view).to.be.ok;
+            });
+
+            it("should target the correct selector for binding", function() {
+                expect(this.view.$el.selector).to.equal('#cluster');
+            });
+
+            it("should be backed by a collection", function(){
+                expect(this.view.collection).to.be.ok;
+            });
+
+            it("should initialize a subviews object", function(){
+                expect(this.view.subviews).to.be.ok;
+                expect(this.view.subviews).to.be.instanceOf(Object);
+            });
+
+            it("should initialize a subview for the pick list", function() {
+                expect(this.view.subviews.picker).to.be.ok;
+            });
+
+            it("should initialize a subview for the index output", function() {
+                expect(this.view.subviews.index).to.be.ok;
+            });
+
+            it('should have an events hash', function () {
+                expect(this.view.events).to.be.ok;
+            });
+
+            it('should have the correct event listeners', function() {
+                var eventsHash = this.view.events;
+                expect(eventsHash['click .modal-footer button']).to.be.ok;
+                expect(eventsHash['click .modal-footer button']).to.equal('closeInputModal');
+                expect(this.view.closeInputModal).to.be.instanceOf(Function);
+                expect(eventsHash['click tr.variant a']).to.be.ok;
+                expect(eventsHash['click tr.variant a']).to.equal('showContextModal');
+                expect(this.view.showContextModal).to.be.instanceOf(Function);
+            }); 
 
             it('should have Mousetrap available', function() {
                 expect(Mousetrap).to.be.ok;
@@ -144,30 +93,66 @@ define([
                     expect(this.spy.withArgs('option+s').calledOnce).to.be.true;
                     expect(this.spy.withArgs('h').calledOnce).to.be.true;
                 });         
-            });            
-            
+            });                       
+        });
+
+        describe('events', function () {
+
+            beforeEach(function () {
+                this.dedupeFingerprintStub = sinon.stub(ClusterView.prototype, 'dedupeFingerprint');
+                this.getFacetsStub = sinon.stub(ClusterView.prototype, 'getFacets');
+                var seedsStub = sinon.createStubInstance(Seeds);
+                this.view = new ClusterView({collection: seedsStub});
+            });
+
+            afterEach(function () {
+                ClusterView.prototype.dedupeFingerprint.restore();
+                ClusterView.prototype.getFacets.restore();
+                this.view.cleanUp();
+            });
+           
+            it("should respond to a seedQuerySuccess event", function() {
+                
+                Backbone.trigger('seedQuerySuccess', 'cup tea');
+                expect(this.dedupeFingerprintStub.callCount).to.equal(1);
+                
+                var stubArgs = this.dedupeFingerprintStub.args[0];
+                expect(stubArgs[0]).to.equal('cup tea');
+
+            });
+
+            it('should respond to a fingerprintSuccess event', function () {
+                
+                Backbone.trigger('fingerprintSuccess', 'cup tea');
+                expect(this.getFacetsStub.callCount).to.equal(1);
+                
+                var stubArgs = this.getFacetsStub.args[0];
+                expect(stubArgs[0]).to.equal('cup tea');
+            });
+
         });
 
         describe('shuffle cluster', function () {
             
             before(function () {
-                this.spy = sinon.spy(ClusterView.prototype, 'resetCluster');
-                this.view = new ClusterView();
+                this.resetClusterStub = sinon.stub(ClusterView.prototype, 'resetCluster');
+                var seedsStub = sinon.createStubInstance(Seeds);
+                this.view = new ClusterView({collection: seedsStub});
             });
 
             after(function () {
                 ClusterView.prototype.resetCluster.restore();
-                this.view.remove();
+                this.view.cleanUp();
                 this.view = null;
             });
 
             it('should shuffle cluster on Mousetrap space event', function () {
-                expect(this.spy).to.not.have.been.called;
+                expect(this.resetClusterStub).to.not.have.been.called;
                 Mousetrap.trigger('space');
-                expect(this.spy.calledOnce).to.be.true;
+                expect(this.resetClusterStub.calledOnce).to.be.true;
                 Mousetrap.trigger('space');
                 Mousetrap.trigger('space');
-                expect(this.spy.calledThrice).to.be.true;
+                expect(this.resetClusterStub.calledThrice).to.be.true;
             });
             
         });
@@ -175,38 +160,31 @@ define([
         describe('dedupe fingerprints', function () {
             
             before(function () {
-                this.server = sinon.fakeServer.create();
-                this.server.respondWith(
-                        "GET",
-                        "http://api.publicfare.org/menus/item/_search"
-                    );
-                this.server.autoRespond = true;
 
-                this.spy = sinon.spy(ClusterView.prototype, 'getFacets');
+                this.getFacetsStub = sinon.stub(ClusterView.prototype, 'getFacets');
                 var seedsCollex = new Seeds();
                 this.view = new ClusterView({collection: seedsCollex});
             });
 
             it('should deduplicate fingerprint values', function () {
                 
-                expect(this.spy).to.not.have.been.called;
+                expect(this.getFacetsStub).to.not.have.been.called;
 
                 this.view.dedupeFingerprint('coffee');
                 this.view.dedupeFingerprint('green peppers stuffed');
                 // Duplicate
                 this.view.dedupeFingerprint('coffee');
 
-                expect(this.spy.callCount).to.equal(2);
+                expect(this.getFacetsStub.callCount).to.equal(2);
                 expect(this.view.collection.length).to.equal(2);
 
             });
 
             after(function () {
-                this.server.restore();
                 ClusterView.prototype.getFacets.restore();
                 this.view.collection.reset();
                 this.view.collection = null;
-                this.view.remove();
+                this.view.cleanUp();
                 this.view = null;
             });
         });
