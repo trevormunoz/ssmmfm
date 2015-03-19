2930f28d795cc2c6ada7706f94333fb78e38dde4
Trevor Muñoz <trevor.munoz@gmail.com>
2015-03-18 22:09:52 -0400

    Passing tests for cluster

--- test/tests/cluster.spec.js
+++ test/tests/cluster.spec.js
@@ -5,7 +5,6 @@ define([
         '../../src/js/models/item.js',
         '../../src/js/collections/cluster.js',
         'fixtures/es/fixture',
-        'helpers/fakeServer-helper'
 ], function(sinon, MenuItem, Cluster) {
     'use strict';
 
@@ -34,10 +33,6 @@ define([
                 expect(this.cluster.model).to.be.ok;
             });
 
-            it("should default url property to es search url", function() {
-                expect(this.cluster.url).to.equal('http://api.publicfare.org/menus/item/_search');
-            });
-
             it("can be instantiated from a collection of models", function() {
 
                 var item1 = new MenuItem({dish_name: "Consomme printaniere royal", dish_id: 1});
@@ -52,57 +47,38 @@ define([
             });
         });
 
-        describe("population from server", function() {
-            
+        describe("fetch and parse", function() {
 
             beforeEach(function() {
                 this.fixture = this.fixtures.Cluster.valid;
-                this.server = sinon.fakeServer.create();
-                this.server.respondWith(
-                        "GET",
-                        "http://api.publicfare.org/menus/item/_search",
-                        this.validResponse(this.fixture)
-                    );
                 this.cluster = new Cluster();
+                var data = this.cluster.parse(this.fixture);
+                this.cluster.reset(data);
             });
 
             afterEach(function() {
-                this.server.restore();
+                this.cluster.reset();
+                this.cluster = null;
             });
 
-            describe("fetch", function() {
+            // TODO test for fetch
 
-                // No application code to implement — making sure the fake server is working
-                it("should make the correct request", function() {
-                    this.cluster.fetch();
-
-                    expect(this.server.requests.length).to.equal(1);
-                    expect(this.server.requests[0].method).to.equal("GET");
-                    expect(this.server.requests[0].url).to.equal("http://api.publicfare.org/menus/item/_search");
-                });
-
-                // This is testing application code
-                it("should parse dishes from the response", function() {
-                    this.cluster.fetch({ reset: true });
-                    this.server.respond();
-
-                    expect(this.cluster).to.have.length(this.fixture.aggregations.dish.buckets.length); 
-                });
-
-                it("should return attributes of fetched dish", function(done) {
-                    this.cluster.fetch({ reset: true });
-                    this.server.respond();
-
-                    var item = this.cluster.at(0);
-                    var fixtureItem = this.fixture.aggregations.dish.buckets[0];
-                    
-                    expect(item.get("id")).to.equal(fixtureItem.key);
-                    expect(item.get("menu_count")).to.equal(fixtureItem.top_names.hits.hits[0]._source.dish_menus_appeared);
-                    expect(item.get("name_value")).to.equal(fixtureItem.top_names.hits.hits[0]._source.dish_name);
-                    expect(item.get("exemplar_doc")).to.equal(fixtureItem.top_names.hits.hits[0]._id);
-                    done();
-                });
+            it('should parse responses from the server', function () {
+                expect(this.cluster).to.have.length(this.fixture.aggregations.dish.buckets.length);
             });
+
+            it('should return attributes of dish', function () {
+
+                var item = this.cluster.at(0);
+                var fixtureItem = this.fixture.aggregations.dish.buckets[0];
+                
+                expect(item.get("id")).to.equal(fixtureItem.key);
+                expect(item.get("menu_count")).to.equal(fixtureItem.top_names.hits.hits[0]._source.dish_menus_appeared);
+                expect(item.get("name_value")).to.equal(fixtureItem.top_names.hits.hits[0]._source.dish_name);
+                expect(item.get("exemplar_doc")).to.equal(fixtureItem.top_names.hits.hits[0]._id);
+                
+            });
+
         });
     });
 });
