c1dc120ec0d5f77202e62efa489b931e74bb5446
Trevor Muñoz <trevor.munoz@gmail.com>
2015-03-18 22:09:28 -0400

    Adjustment to fixture data to be valid

--- test/fixtures/es/fixture.js
+++ test/fixtures/es/fixture.js
@@ -6,8 +6,113 @@ define(['mocha'], function() {
   mocha.setup("bdd");
 
     beforeEach(function() {
-      
+
       this.fixtures = {
+
+        Cluster: {
+
+          valid: {
+                "took": 65,
+                "timed_out": false,
+                "_shards": {
+                    "total": 5,
+                    "successful": 5,
+                    "failed": 0
+                },
+                "hits": {
+                    "total": 724,
+                    "max_score": 0,
+                    "hits": []
+                },
+                "aggregations": {
+                    "dish": {
+                        "doc_count_error_upper_bound": 0,
+                        "sum_other_doc_count": 0,
+                        "buckets": [
+                            {
+                                "key": "691.0",
+                                "doc_count": 622,
+                                "top_names": {
+                                    "hits": {
+                                        "total": 622,
+                                        "max_score": null,
+                                        "hits": [
+                                            {
+                                                "_index": "menus",
+                                                "_type": "item",
+                                                "_id": "7227",
+                                                "_score": null,
+                                                "_source": {
+                                                    "dish_menus_appeared": 613,
+                                                    "menu_page_uri": "http://menus.nypl.org/menu_pages/304",
+                                                    "dish_name": "Lamb Chops"
+                                                },
+                                                "sort": [
+                                                    613
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+                            {
+                                "key": "373520.0",
+                                "doc_count": 72,
+                                "top_names": {
+                                    "hits": {
+                                        "total": 72,
+                                        "max_score": null,
+                                        "hits": [
+                                            {
+                                                "_index": "menus",
+                                                "_type": "item",
+                                                "_id": "1305412",
+                                                "_score": null,
+                                                "_source": {
+                                                    "dish_menus_appeared": 72,
+                                                    "menu_page_uri": "http://menus.nypl.org/menu_pages/44272",
+                                                    "dish_name": "Lamb chops"
+                                                },
+                                                "sort": [
+                                                    72
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+                            {
+                                "key": "424809.0",
+                                "doc_count": 8,
+                                "top_names": {
+                                    "hits": {
+                                        "total": 8,
+                                        "max_score": null,
+                                        "hits": [
+                                            {
+                                                "_index": "menus",
+                                                "_type": "item",
+                                                "_id": "1325004",
+                                                "_score": null,
+                                                "_source": {
+                                                    "dish_menus_appeared": 6,
+                                                    "menu_page_uri": "http://menus.nypl.org/menu_pages/39712",
+                                                    "dish_name": "LAMB CHOPS"
+                                                },
+                                                "sort": [
+                                                    6
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+                        ]
+                    }
+                }
+            }
+          
+        }
 
       };
 
