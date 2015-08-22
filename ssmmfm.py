0f366738542739185838064ced99040bd39edaa1
trevormunoz <trevor.munoz@gmail.com>
2015-08-22 16:12:52 -0600

    pep8 fixes

--- ssmmfm.py
+++ ssmmfm.py
@@ -1,11 +1,13 @@
 from requests_oauthlib import OAuth2Session
-from flask import Flask, request, render_template, render_template, redirect, session, url_for
+from flask import Flask, request, render_template, redirect, session, url_for
 from werkzeug.contrib.fixers import ProxyFix
 from flask.json import jsonify
-import os, json
+import os
+import json
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 
 @app.route('/')
 def index():
@@ -14,13 +16,15 @@ def index():
 
 @app.route('/robots.txt')
 def robots():
-     return app.send_static_file('robots.txt')
+    return app.send_static_file('robots.txt')
 
 
 @app.route("/login")
 def login():
-    github = OAuth2Session(app.config['CLIENT_ID'], scope=['public_repo', 'write:org'])
-    authorization_url, state = github.authorization_url(app.config['AUTH_BASE_URL'])
+    github = OAuth2Session(app.config['CLIENT_ID'],
+                           scope=['public_repo', 'write:org'])
+    authorization_url, state = github.authorization_url(
+        app.config['AUTH_BASE_URL'])
 
     session['oauth_state'] = state
     return redirect(authorization_url)
@@ -28,10 +32,11 @@ def login():
 
 @app.route("/callback", methods=["GET"])
 def callback():
-    github = OAuth2Session(app.config['CLIENT_ID'], state=session['oauth_state'])
+    github = OAuth2Session(app.config['CLIENT_ID'],
+                           state=session['oauth_state'])
     token = github.fetch_token(app.config['TOKEN_URL'],
-                                    client_secret=app.config['CLIENT_SECRET'],
-                                    authorization_response=request.url)
+                               client_secret=app.config['CLIENT_SECRET'],
+                               authorization_response=request.url)
 
     session['oauth_token'] = token
     return redirect(url_for('.ssmmfm'))
@@ -47,7 +52,8 @@ def ssmmfm():
 
 @app.route("/user", methods=["GET"])
 def user_data():
-    github = OAuth2Session(app.config['CLIENT_ID'], token=session['oauth_token'])
+    github = OAuth2Session(app.config['CLIENT_ID'],
+                           token=session['oauth_token'])
     results = github.get('https://api.github.com/user').json()
 
     return jsonify(results)
@@ -55,22 +61,29 @@ def user_data():
 
 @app.route("/reviews", methods=["GET", "POST"])
 def create_issue():
-    github = OAuth2Session(app.config['CLIENT_ID'], token=session['oauth_token'])
+    github = OAuth2Session(app.config['CLIENT_ID'],
+                           token=session['oauth_token'])
     token = 'token:{0}'.format(github.token['access_token'])
-    headers = {'Authorization': token, 'Accept': 'application/vnd.github.moondragon+json'}
+    headers = {'Authorization': token,
+               'Accept': 'application/vnd.github.moondragon+json'}
 
     if request.method == 'POST':
         body = render_template('issue.md',
-                                    fingerprint=request.json['body']['fingerprint'],
-                                    desc=request.json['body']['description'],
-                                    links=request.json['body']['links'])
+                               fingerprint=request.json['body']['fingerprint'],
+                               desc=request.json['body']['description'],
+                               links=request.json['body']['links'])
         issue = {'title': request.json['title'], 'body': body}
-        
-        response = github.post('https://api.github.com/repos/public-fare/data/issues', data=json.dumps(issue), headers=headers)
-        
+
+        response = github.post(
+            'https://api.github.com/repos/public-fare/data/issues',
+            data=json.dumps(issue),
+            headers=headers
+        )
+
         return jsonify(response.json())
     elif request.method == 'GET':
-        response = github.get('https://api.github.com/repos/public-fare/data/issues').json()
+        response = github.get(
+            'https://api.github.com/repos/public-fare/data/issues').json()
         return jsonify({'issues': response})
     else:
         return redirect(url_for('.server_error'))
@@ -79,6 +92,7 @@ def create_issue():
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html')
+
 
 @app.errorhandler(500)
 def server_error(error):
